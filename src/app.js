14e5ac9bfb814aa7e56e7607925acc697cc84d7d
Sunpyo Hong <sh4023@nyu.edu>
2021-04-08 22:35:33 +0900

    update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 // app.js
+
+
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, 'public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000);
