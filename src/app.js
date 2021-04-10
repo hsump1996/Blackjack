2b172ebcada759a8db135346589e4f6c48d0f1bc
Sunpyo Hong <sh4023@nyu.edu>
2021-04-10 21:33:18 +0900

    update

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@
 
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-
 
 const publicPath = path.resolve(__dirname, 'public');
 
--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,22 +1,136 @@
 function main(){
 
-
+    const userInput = [];
 
     document.querySelector('.playBtn').addEventListener('click', function(event) {
         
         event.preventDefault();
 
-        const userInput = [];
+        let startDiv = document.querySelector("div.start");
 
-        const deck = [];
+        startDiv.style.display = "none";
+
+
+        document.getElementById('startValues').value.split(',').forEach((value) => {
+            
+            userInput.push(value)
+        
+        });
+        
+        let deck = [];
 
         const temp_deck = [];
 
-        const suits = ['♠','♣','♥', '♦'];
+
+        let userTotal = 0;
+
+        let computerTotal = 0;
+
+        for (i = 0; i < userInput.length; i++) {
+
+            if (i == 1 || i == 3) {
+
+                if (userInput[i] == 'J' || userInput[i] == 'Q' || userInput[i] == 'K') {
+
+                    userTotal += 10;
+    
+                } else if (userInput[i] == 'A'){
+
+                    userTotal += 11;
+
+                } else {
+                    
+                    userTotal += parseInt(userInput[i]);
+                }
+            } else if (i == 0 || i == 2) {
+
+                if (userInput[i] == 'J' || userInput[i] == 'Q' || userInput[i] == 'K') {
+
+                    computerTotal += 10;
+    
+                } else if (userInput[i] == 'A'){
+
+                    computerTotal += 11;
+
+                } else {
+                    
+                    computerTotal += parseInt(userInput[i]);
+                }
+
+            }
+        }
+
+        const arrayofImages = ['./img/AS.jpg', './img/AC.jpg', './img/AH.jpg','./img/AD.jpg', './img/KS.jpg', './img/KC.jpg', 
+        './img/KH.jpg','./img/KD.jpg', './img/QS.jpg', './img/QC.jpg', './img/QH.jpg','./img/QD.jpg','./img/JS.jpg', './img/JC.jpg', './img/JH.jpg','./img/JD.jpg', 
+        './img/10S.jpg', './img/10C.jpg','./img/10H.jpg', './img/10D.jpg', './img/9S.jpg','./img/9C.jpg', './img/9H.jpg', './img/9D.jpg', './img/8S.jpg','./img/8C.jpg', './img/8H.jpg', './img/8D.jpg',
+        './img/7S.jpg','./img/7C.jpg', './img/7H.jpg', './img/7D.jpg', './img/6S.jpg','./img/6C.jpg', './img/6H.jpg', './img/6D.jpg', './img/5S.jpg','./img/5C.jpg', './img/5H.jpg', './img/5D.jpg',
+        './img/4S.jpg','./img/4C.jpg', './img/4H.jpg', './img/4D.jpg', './img/3S.jpg','./img/3C.jpg', './img/3H.jpg', './img/3D.jpg', './img/2S.jpg','./img/2C.jpg', './img/2H.jpg', './img/2D.jpg'];
+
+
+        //Creates the first div
+        let div1 = document.createElement('div');
         
-        document.querySelector('#startValues').value.split(',').forEach((value) => {
-            startValues.push(value)
-        });
+        div1.setAttribute("id", "computerHandDisplay");
+        
+        document.querySelector("div.game").appendChild(div1);
+
+        let computerHandHeader = document.createElement("h1");
+		
+        let computerHandHeaderContent = document.createTextNode("Computer Hand - Total: ?");
+		
+        computerHandHeader.appendChild(computerHandHeaderContent);
+
+        document.getElementById("computerHandDisplay").appendChild(computerHandHeader);
+
+
+
+        //Creates the second div
+        let div2 = document.createElement('div');
+        
+        div2.setAttribute("id", "computerHand");
+
+        document.querySelector("div.game").appendChild(div2);
+
+        
+        //Creates the third div
+        let div3 = document.createElement('div');
+        
+        div3.setAttribute("id", "playerHandDisplay");
+
+
+        document.querySelector("div.game").appendChild(div3);
+
+        let playerHandHeader = document.createElement("h1");
+		
+        let playerHandHeaderConent = document.createTextNode("Player Hand - Total: " + userTotal);
+		
+        playerHandHeader.appendChild(playerHandHeaderConent);
+
+        document.getElementById("playerHandDisplay").appendChild(playerHandHeader);
+
+
+        //Creates the fourth div
+        let div4 = document.createElement('div');
+        
+        div4.setAttribute("id", "playerHand");
+
+        document.querySelector("div.game").appendChild(div4);
+
+
+        //Creates the fifth div with buttons
+        let div5 = document.createElement('div');
+        
+        div5.setAttribute("id", "buttons");
+
+        document.querySelector("div.game").appendChild(div5);
+
+        let hitButton = document.createElement("button");
+        hitButton.appendChild(document.createTextNode("Hit"));
+        document.getElementById("buttons").appendChild(hitButton);
+        
+        let standButton = document.createElement("button");
+        standButton.appendChild(document.createTextNode("Stand"));
+        document.getElementById("buttons").appendChild(standButton);
 
         // Initially fills the deck with userInput
         for (i = 0 ; i < userInput.length; i++) {
@@ -25,22 +139,21 @@ function main(){
 
             if (suit == 0) {
 
-                deck.push(userInput[i] + "♠");
+                deck.push(userInput[i] + "S");
 
             } else if (suit == 1) {
 
-                deck.push(userInput[i] + "♣");
+                deck.push(userInput[i] + "C");
 
             } else if (suit == 2) {
 
-                deck.push(userInput[i] + "♥");
+                deck.push(userInput[i] + "H");
 
             } else {
 
-                deck.push(userInput[i] + "♦");
+                deck.push(userInput[i] + "D");
             }
         }
-
 
         // Fills the temp_deck with 52 cards with all the suits
         for (i = 2; i <= 14; i++) {
@@ -73,33 +186,69 @@ function main(){
 
                 if (j == 0) {
 
-                    temp_deck.push(cardFace + "♠");
+                    temp_deck.push(cardFace + "S");
 
                 } else if (j == 1) {
 
-                    temp_deck.push(cardFace + "♣");
+                    temp_deck.push(cardFace + "C");
 
                 } else if (j == 2) {
 
-                    temp_deck.push(cardFace + "♥");
+                    temp_deck.push(cardFace + "H");
 
                 } else {
 
-                    temp_deck.push(cardFace + "♦");
+                    temp_deck.push(cardFace + "D");
 
                 }
             }
         }
 
+        // Combines the deck(currently with user input) with temp_deck
+        for (i = 0; i < temp_deck.length; i++) {
+
+            if (!deck.includes(temp_deck[i])) {
+                deck.push(temp_deck[i]);
         
+            }
+        
+        }
 
+        // Sorts the deck from the 5th card to the end
+        deck2 = randomSort(deck);
 
+        for (j = 0; j < deck2.length; j++) {
+
+            if (j >= 0 && j < 4) {
+
+                for (i = 0; i < arrayofImages.length; i++) {
+
+                    if (arrayofImages[i].includes(deck2[j])) {
+                        
+                        let newImg = document.createElement("img");
+                        
+                        newImg.src = arrayofImages[i];
+
+                        newImg.height = "150";
+                        newImg.width = "200";
+                        
+                        if (j == 0 || j == 2) {
+
+                            document.getElementById("computerHand").appendChild(newImg);
+
+                        } else {
+
+                            document.getElementById("playerHand").appendChild(newImg);
+                        }                        
+                        break;
+                    
+                    }
+                }
+            } 
+        }
     
-
-
     });
-
-}
+};
 
 
 function getRandomInt(max) {
@@ -107,5 +256,33 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 
 }
+
+function randomSort(array) {
+
+    let randomInteger;
+    let temp = -1;
+
+    for (i = 0; i < array.length-1; i++) {
+
+        if (i > 3) {
+            randomInteger = -1;
+            
+            while (randomInteger <= 3) {
+                
+                randomInteger = Math.floor(Math.random() * array.length);
+            
+            }
+            
+            temp = array[i];
+        
+            array[i] = array[randomInteger];
+
+            array[randomInteger] = temp;
+        }
+    }
+
+    return array;
+}
+
 
 document.addEventListener('DOMContentLoaded', main);
